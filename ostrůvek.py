85e1a36ecec1ff77af288dc784285c732ea7b047
kokos <onik.henzl@gmail.com>
2024-04-30 17:31:34 +0200

    makedonie 1.8

---
+++ ostrůvek.py
@@ -0,0 +1,15 @@
+import random
+
+seznam_cisel = []
+maximum = 30
+pocet = 5
+
+for i in range(pocet):
+    seznam_cisel.append(random.randint(0, maximum))
+
+print(seznam_cisel)
+
+def setrid(seznam):
+    pass
+
+print(setrid(seznam_cisel))
