81522435d24d601fec1453fdf2c45d86bfdd0067
Ondrej Henzl <onikhenzl@gmail.com>
2025-02-02 14:38:23 +0100

    hezky zarovnane

--- makedonie/makedonie.py
+++ makedonie/makedonie.py
@@ -4,8 +4,34 @@ init()
 
 mistnosti_co_hoří = []
 kam_lze_jit_a_hori = []
-mistnosti = ["0-obývák", "1-chodba", "2-sklep", "3-trůnní sál", "4-jídelna","5-terasa","6-půda","7-kumbál","8-sídlo","9-ložnice","10-chodba","11-tajemna komnata"]
-chodby = [[1, 2,4,], [0,4,6], [0,11], [1, 2],[1,0,5,7],[4,6,10],[1,5],[4],[10],[10],[9,8,5],[2]]
+mistnosti = [
+            "0-obývák",
+            "1-chodba",
+            "2-sklep",
+            "3-trůnní sál",
+            "4-jídelna",
+            "5-terasa",
+            "6-půda",
+            "7-kumbál",
+            "8-sídlo",
+            "9-ložnice",
+            "10-chodba",
+            "11-tajemna komnata"
+            ]
+chodby = [
+            [1, 2, 4],      # "0-obývák"
+            [0,4,6],        # "1-chodba"
+            [0,11],         # "2-sklep"
+            [1, 2],         # "3-trůnní sál"
+            [1,0,5,7],      # "4-jídelna"
+            [4,6,10],       # "5-terasa"
+            [1,5],          # "6-půda"
+            [4],            # "7-kumbál"
+            [10],           # "8-sídlo"
+            [10],           # "9-ložnice"
+            [9,8,5],        # "10-chodba"
+            [2]             # "11-tajemna komnata"
+        ]
 zamcene_chodby = [[], [3], [], [],[],[],[],[],[],[],[],[],[]]
 inventar = {"klic": False,
             "moje zlato": 0,
@@ -19,7 +45,7 @@ mistnost_s_klicem = random.choice([1,2,5,6,7,9,10])
 mistnost_s_hasicakem = 2
 mistnost_s_pasti = random.choice([1,7])
 zlato = [1, 0, 10, 100, 15,20,3,0,50,30,0,10]
-hrac = 0
+mistnost_s_hracem = 0
 skore = 0
 kroky = 0
 prisera = None
@@ -66,28 +92,28 @@ while not hotovo():
     if inventar["hulka"]:
         print("mas hulku")
     print("mas kroky", [kroky])
-    if hrac in mistnosti_co_hoří:
+    if mistnost_s_hracem in mistnosti_co_hoří:
         print(Fore.RED + "bůh vám vskazuje že jste uhořel")
     if prisera is not None:
         print(Fore.YELLOW + "prisera je v mistnosti " + mistnosti[prisera])
         print(Style.RESET_ALL)
-    print("hráč je v místnosti:", mistnosti[hrac])
+    print("hráč je v místnosti:", mistnosti[mistnost_s_hracem])
     print("hráč má", skore, "zlata")
     print("zbývá zlata:", sum(zlato))
-    kam_lze_jit = chodby[hrac]
+    kam_lze_jit = chodby[mistnost_s_hracem]
     pada_meteorit = False
-    if random.random() < 0.2 and hrac != 2 and kroky > 3:
+    if random.random() < 0.2 and mistnost_s_hracem != 2 and kroky > 3:
         print(Fore.RED + "pozor padá meteorit")
         print(Style.RESET_ALL)
         pada_meteorit = True
-    if random.random() < 0.75 and hrac == mistnost_s_pasti and prisera is None:
+    if random.random() < 0.75 and mistnost_s_hracem == mistnost_s_pasti and prisera is None:
         print(Fore.RED + "šlápl jsi na past a vypustil jsi priseru")
         print(Style.RESET_ALL)
         prisera = 2
-    if hrac == mistnost_s_voldemortem:
+    if mistnost_s_hracem == mistnost_s_voldemortem:
         print(Fore.RED + "pozor jsi v místnosti z voldemortem")
         print(Style.RESET_ALL)
-    if hrac == mistnost_s_baziliskem:
+    if mistnost_s_hracem == mistnost_s_baziliskem:
         print(Fore.RED + "pozor jsi v místnosti z baziliskem")
         print(Style.RESET_ALL)
 
@@ -102,25 +128,25 @@ while not hotovo():
     # moznosti
     for i, moznost in enumerate(kam_lze_jit):
         print("Moznost", i + 1, ": ", mistnosti[moznost])
-    if zlato[hrac] > 0:
-        print("Moznost X : sebrat", zlato[hrac], "zlata")
-    if hrac == mistnost_s_klicem:
+    if zlato[mistnost_s_hracem] > 0:
+        print("Moznost X : sebrat", zlato[mistnost_s_hracem], "zlata")
+    if mistnost_s_hracem == mistnost_s_klicem:
         print("Moznost C : sebrat klíč")
-    if hrac == mistnost_s_hasicakem:
+    if mistnost_s_hracem == mistnost_s_hasicakem:
         print("Moznost H : sebrat hasicak")
-    if hrac == mistnost_s_náhrdelnikem and inventar["hulka"]:
+    if mistnost_s_hracem == mistnost_s_náhrdelnikem and inventar["hulka"]:
         print ("moznost Z : zničit nahrdelnik")
-    if inventar["klic"] and zamcene_chodby[hrac]:
-        print("Moznost R : odemknout dveře ->", mistnosti[zamcene_chodby[hrac][-1]])
-    if hrac == mistnost_s_voldemortem and inventar["hulka"] and mistnost_s_baziliskem == None and mistnost_s_náhrdelnikem == None:
+    if inventar["klic"] and zamcene_chodby[mistnost_s_hracem]:
+        print("Moznost R : odemknout dveře ->", mistnosti[zamcene_chodby[mistnost_s_hracem][-1]])
+    if mistnost_s_hracem == mistnost_s_voldemortem and inventar["hulka"] and mistnost_s_baziliskem == None and mistnost_s_náhrdelnikem == None:
         print("Moznost A : zabít voldemorta")
-    if hrac == mistnost_s_baziliskem and inventar["hulka"]:
+    if mistnost_s_hracem == mistnost_s_baziliskem and inventar["hulka"]:
         print("Moznost G : zabít baziliska")
-    if hrac == 4 or inventar["burger"]:
+    if mistnost_s_hracem == 4 or inventar["burger"]:
         print("moznost J: najíst se")
-    if hrac == 4 and inventar["moje zlato"] >= cena_burgeru:
+    if mistnost_s_hracem == 4 and inventar["moje zlato"] >= cena_burgeru:
         print("prodavačka říká:Chceš burger?řekni b")
-    if hrac == mistnost_s_hulkou:
+    if mistnost_s_hracem == mistnost_s_hulkou:
         print("Moznost I : sebrat hulku")
     if not (kam_lze_jit_a_hori == []) and inventar["hasicak"]:
         print("moznost U : uhasit mistnosti kolem co hoří")
@@ -136,27 +162,27 @@ while not hotovo():
     vstup_ok=False
     while not vstup_ok:
         vstup = input("> ")
-        if not inventar["klic"] and mistnost_s_klicem==hrac and vstup=="c" :
+        if not inventar["klic"] and mistnost_s_klicem==mistnost_s_hracem and vstup=="c" :
             vstup_ok=True
-        elif not inventar["hasicak"]  and mistnost_s_hasicakem==hrac and vstup=="h":
+        elif not inventar["hasicak"]  and mistnost_s_hasicakem==mistnost_s_hracem and vstup=="h":
             vstup_ok=True
-        elif zlato[hrac] and vstup=="x":
+        elif zlato[mistnost_s_hracem] and vstup=="x":
             vstup_ok = True
-        elif hrac == mistnost_s_hulkou and vstup =="i":
+        elif mistnost_s_hracem == mistnost_s_hulkou and vstup =="i":
             vstup_ok=True
-        elif hrac == mistnost_s_náhrdelnikem and inventar["hulka"] and vstup =="z":
+        elif mistnost_s_hracem == mistnost_s_náhrdelnikem and inventar["hulka"] and vstup =="z":
             vstup_ok=True
-        elif hrac == mistnost_s_voldemortem and inventar["hulka"] and mistnost_s_baziliskem == None and mistnost_s_náhrdelnikem == None and vstup == "a":
+        elif mistnost_s_hracem == mistnost_s_voldemortem and inventar["hulka"] and mistnost_s_baziliskem == None and mistnost_s_náhrdelnikem == None and vstup == "a":
             vstup_ok = True
-        elif hrac == mistnost_s_baziliskem and inventar["hulka"] and vstup == "g":
+        elif mistnost_s_hracem == mistnost_s_baziliskem and inventar["hulka"] and vstup == "g":
             vstup_ok = True
         elif vstup=="u" :
             vstup_ok=True
-        elif (hrac==4 or inventar["burger"]) and vstup=="j" :
+        elif (mistnost_s_hracem==4 or inventar["burger"]) and vstup=="j" :
             vstup_ok=True
-        elif vstup=="r" and hrac==1 and zamcene_chodby[hrac]:
+        elif vstup=="r" and mistnost_s_hracem==1 and zamcene_chodby[mistnost_s_hracem]:
             vstup_ok=True
-        elif hrac == 4 and inventar["moje zlato"] >= cena_burgeru and not inventar["burger"] and vstup=="b":
+        elif mistnost_s_hracem == 4 and inventar["moje zlato"] >= cena_burgeru and not inventar["burger"] and vstup=="b":
             vstup_ok=True
         elif je_cislo(vstup) and int(vstup) <= len(kam_lze_jit) and int(vstup) > 0 :
             vstup_ok=True
@@ -169,15 +195,15 @@ while not hotovo():
 
     #vyhodnocovani
     if pada_meteorit and vstup in ["x" , "k"  , "j" ,"b" , "r", "h", "u","i","c","a","z","g"]:
-        print(Fore.RED + "bůh vám skazuje že jte umřel při tragické nehodě pádu meteoritu")
+        print(Fore.RED + "bůh vám vzkazuje, že jste umřel při tragické nehodě pádu meteoritu")
         print(Style.RESET_ALL)
         break
     if pada_meteorit and vstup in ["1" , "2"  , "3" ,"4","5","6"]:
-        mistnosti_co_hoří.append(hrac)
+        mistnosti_co_hoří.append(mistnost_s_hracem)
     if vstup == "x":
-        skore += zlato[hrac]
-        inventar["moje zlato"]+=zlato[hrac]
-        zlato[hrac] = 0
+        skore += zlato[mistnost_s_hracem]
+        inventar["moje zlato"]+=zlato[mistnost_s_hracem]
+        zlato[mistnost_s_hracem] = 0
     elif vstup == "u":
         mistnosti_co_hoří = []
         kam_lze_jit_a_hoři = []
@@ -192,7 +218,7 @@ while not hotovo():
         mistnost_s_náhrdelnikem = None
     elif vstup=="j":
         sytost=11
-        if not hrac==4:
+        if not mistnost_s_hracem==4:
             inventar["burger"]=False
     elif vstup == "b":
         inventar["burger"]=True
@@ -203,33 +229,34 @@ while not hotovo():
         inventar["hulka"] = True
         mistnost_s_hulkou = -1
     elif vstup == "r":
-        cilova_mistnost = zamcene_chodby[hrac].pop()
-        chodby[hrac].append(cilova_mistnost)
+        cilova_mistnost = zamcene_chodby[mistnost_s_hracem].pop()
+        chodby[mistnost_s_hracem].append(cilova_mistnost)
         print("Slyšíš jak cvaknul zámek a dveře se otevřely.")
 
     else:
-        hrac = kam_lze_jit[int(vstup) - 1]
+        # tady se hrac pohne
+        mistnost_s_hracem = kam_lze_jit[int(vstup) - 1]
 
-    if hrac == prisera:
-        print(Fore.RED + "bůh vám skazuje že si vás dala prisera ke svace")
+    if mistnost_s_hracem == prisera:
+        print(Fore.RED + "bůh vám vzkazuje, že si vás dala prisera ke svace")
         print(Style.RESET_ALL)
         break
-    sytost-=1
+    sytost -= 1
     kroky += 1
     if sytost == 0:
-        print(Fore.RED + "bůh vám skazuje že jte umřel hlady")
+        print(Fore.RED + "bůh vám vzkazuje, že jste umřel hlady")
         print(Style.RESET_ALL)
         break
-    if hrac in mistnosti_co_hoří:
-        print(Fore.RED + "bůh vám vskazuje že jste uhořel")
+    if mistnost_s_hracem in mistnosti_co_hoří:
+        print(Fore.RED + "bůh vám vzkazuje, že jste uhořel")
         print(Style.RESET_ALL)
         break
-    if hrac == mistnost_s_voldemortem and vstup in ["x" , "k"  , "j" ,"b" , "r", "h", "u","i","c","z"]:
-        print(Fore.RED + "Brumbál vám vskazuje že na vás byla uvrhnuta kledba AVADA KEDAVRA")
+    if mistnost_s_hracem == mistnost_s_voldemortem and vstup in ["x", "k", "j", "b", "r", "h", "u", "i", "c", "z"]:
+        print(Fore.RED + "Brumbál vám vzkazuje, že na vás byla uvrhnuta kledba AVADA KEDAVRA")
         print(Style.RESET_ALL)
         break
-    if hrac == mistnost_s_baziliskem and vstup in ["x" , "k"  , "j" ,"b" , "r", "h", "u","i","c","z"]:
-        print(Fore.RED + "Brumbál vám vskazuje že si vás dal bazilišek jako dezert")
+    if mistnost_s_hracem == mistnost_s_baziliskem and vstup in ["x", "k", "j", "b", "r", "h", "u", "i", "c", "z"]:
+        print(Fore.RED + "Brumbál vám vzkazuje, že si vás dal bazilišek jako dezert")
         print(Style.RESET_ALL)
         break
     if prisera is not None:
